c532f41f38e46e51f07b8209c187007f64e15b5c
Егор Смирнов <egorsmirn@outlook.com>
2022-05-01 17:02:05 +0400

    Solve task_01_1

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,43 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { defineComponent, createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const component = defineComponent({
+  data() {
+    return {
+      operand1: 0,
+      operand2: 0,
+      fnc(a, b) {
+        return a + b;
+      }
+    };
+  },
+  computed: {
+    operationResult() {
+      return this.fnc(this.operand1, this.operand2);
+    }
+  },
+  template: `
+    <div class="row">
+        <div class="col">
+          <input type="number" v-model="this.operand1" />
+        </div>
+
+        <div class="col" style="display: flex; flex-direction: column; font-family: emoji">
+          <label><input type="radio" name="operator" value="sum" /> ➕</label>
+          <label><input type="radio" name="operator" value="subtract" /> ➖</label>
+          <label><input type="radio" name="operator" value="multiply" /> ✖</label>
+          <label><input type="radio" name="operator" value="divide" /> ➗</label>
+        </div>
+
+        <div class="col">
+          <input type="number" v-model="this.operand2"/>
+        </div>
+
+        <div class="col">=</div>
+
+        <output class="col" :value="operationResult"></output>
+      </div>
+  `
+});
+
+const app = createApp(component);
+app.mount("#app");
