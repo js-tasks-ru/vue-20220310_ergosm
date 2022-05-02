87bba6fc37f460fb480f17eead66174e197d0c0a
Егор Смирнов <egorsmirn@outlook.com>
2022-05-02 20:30:23 +0400

    Solve task_02_07

--- 02-components/07-PageMeetup/PageMeetup.js
+++ 02-components/07-PageMeetup/PageMeetup.js
@@ -1,7 +1,14 @@
 import { defineComponent } from './vendor/vue.esm-browser.js';
 import UiContainer from './UiContainer.js';
 import UiAlert from './UiAlert.js';
-// import { fetchMeetupById } from './meetupService.js';
+import { fetchMeetupById } from './meetupService.js';
+
+import MeetupCover from "../03-MeetupCover/MeetupCover";
+import MeetupDescription from "../02-MeetupDescription/MeetupDescription";
+import MeetupInfo from "../04-MeetupInfo/MeetupInfo";
+import MeetupAgenda from "../05-MeetupAgenda/MeetupAgenda";
+import MeetupAgendaItem from "../05-MeetupAgenda/MeetupAgendaItem";
+import MeetupView from '../06-MeetupView/MeetupView.js';
 
 export default defineComponent({
   name: 'PageMeetup',
@@ -9,18 +16,59 @@ export default defineComponent({
   components: {
     UiAlert,
     UiContainer,
+    MeetupCover,
+    MeetupDescription,
+    MeetupInfo,
+    MeetupAgenda,
+    MeetupAgendaItem,
+    MeetupView
+  },
+
+  data() {
+    return {
+      meetup: null,
+
+      loadError: false
+    }
+  },
+
+  props: {
+    meetupId: {
+      type: Number,
+      required: true
+    }
+  },
+
+  methods: {
+    async getMeetup() {
+      try{
+        this.loadError = null;
+        this.meetup = null;
+        this.meetup = await fetchMeetupById(this.meetupId);
+      }
+      catch{
+        this.loadError = true;
+      }
+    }
+  },
+
+  watch: {
+    async meetupId(newValue, oldValue) {
+      return await this.getMeetup();
+    }
   },
 
   template: `
     <div class="page-meetup">
-      <!-- meetup view -->
+      <MeetupView v-if="meetup !== null" :meetup="meetup" />
 
-      <ui-container>
+      <ui-container v-else-if="!loadError">
         <ui-alert>Загрузка...</ui-alert>
       </ui-container>
 
-      <ui-container>
+      <ui-container v-if="loadError">
         <ui-alert>error</ui-alert>
       </ui-container>
-    </div>`,
+    </div>
+    `,
 });
