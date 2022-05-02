f4884bd09ef28ce1e1564b7d50059e15848f26c6
Егор Смирнов <egorsmirn@outlook.com>
2022-05-02 16:52:38 +0400

    Solve task_02_05

--- 02-components/05-MeetupAgenda/MeetupAgendaItem.js
+++ 02-components/05-MeetupAgenda/MeetupAgendaItem.js
@@ -13,12 +13,14 @@ export default defineComponent({
 
   computed: {
     icon: function() {
-      console.log(agendaItemIcons[this.agendaItem.type]);
       return agendaItemIcons[this.agendaItem.type];
+    },
+
+    title: function() {
+      return this.agendaItem.title ?? agendaItemDefaultTitles[this.agendaItem.type];
     }
   },
 
-//${agendaItemIcons[this.agendaItem.type]}
   template: `
     <div class="agenda-item">
       <div class="agenda-item__col">
@@ -26,7 +28,7 @@ export default defineComponent({
       </div>
       <div class="agenda-item__col">{{ agendaItem.startsAt }} - {{ agendaItem.endsAt }}</div>
       <div class="agenda-item__col">
-        <h3 class="agenda-item__title">{{ agendaItem.title }}</h3>
+        <h3 class="agenda-item__title">{{ title }}</h3>
         <p class="agenda-item__talk" v-if="agendaItem.type === 'talk'">
           <span>{{ agendaItem.speaker }}</span>
           <span class="agenda-item__dot"></span>
