67fbf2dcdee1779c2151c8570b80ea248069c7d6
Егор Смирнов <egorsmirn@outlook.com>
2022-05-12 13:34:32 +0400

    Solve task 05_01

--- 05-vue-router/01-AuthPages/views/PageRegister.vue
+++ 05-vue-router/01-AuthPages/views/PageRegister.vue
@@ -31,9 +31,9 @@
           </label>
         </ui-form-group>
         <div class="form__buttons">
-          <button type="submit" class="button button_primary">Зарегистрироваться</button>
+          <button type="submit" @click="handleSubmit" class="button button_primary">Зарегистрироваться</button>
         </div>
-        <div class="form__append">Уже есть аккаунт? <a href="/login" class="link">Войдите</a></div>
+        <div class="form__append">Уже есть аккаунт? <router-link to="/login">Войдите</router-link></div>
       </form>
     </ui-container>
   </div>
@@ -53,7 +53,7 @@ export default {
 
   methods: {
     handleSubmit() {
-      // Требуется обработать сабмит формы
+      this.$router.push('login');
     },
   },
 };
